06966ed0a29000c120002273f0bad07d249aa76e
PaolaDaft <andrepaolagd@gmail.com>
2022-05-04 13:56:02 -0500

    S2 Prueba 2do requerimiento|Passed

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -4,6 +4,10 @@ class UserService {
     static create (id, username, name) {
         return new User(id, username, name, "sin bio")
     }
+
+    static getInfo(user) {
+        return Object.values(user).slice(0, 4)
+    }
 }
 
 module.exports = UserService
