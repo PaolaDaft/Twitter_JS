53e721320c242f33e8f24276598ce48f30cc9eb2
PaolaDaft <andrepaolagd@gmail.com>
2022-05-04 07:41:38 -0500

    S2 Prueba 1er requerimiento|Passed

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -1,8 +1,9 @@
 const UserService = require('./../../app/services/UserService');
 
 describe("Tests for UserService", () => {
+
     test('1. Create a new user using the UserService', () => {
-        const user = new UserService.create(1, "paola_daft", "Paola")
+        const user = UserService.create(1, "paola_daft", "Paola")
         expect(user.username).toBe("paola_daft");
         expect(user.name).toBe("Paola");
         expect(user.id).toBe(1);
