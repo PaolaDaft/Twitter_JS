aa6e02446e397f7284f3ba532f71022a71498472
PaolaDaft <andrepaolagd@gmail.com>
2022-05-05 09:19:29 -0500

    S3 Prueba 4to requerimiento|Passed

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -4,11 +4,12 @@ class UserView {
     static createUser(payload){
         if (!payload) {
             return { error: 'payload no existe' }
-        }else if (typeof payload.userName !== 'string' & typeof payload.name !== 'string' & typeof payload.id !== 'number'){
+        }else if (typeof payload.userName !== 'string' && typeof payload.name !== 'string' && typeof payload.id !== 'number'){
             return { error: 'necesitan tener un valor válido' }
-        }
+        }else{
+            return UserService.create(payload.id, payload.username, payload.name)
 
+        }
     }
-    
 }
 module.exports = UserView
